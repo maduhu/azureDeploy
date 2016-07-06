8ee2d318219208849b65dad15200237c163483a9
Scott Kahler <skahler@pivotal.io>
2016-07-06 11:30:52 -0500

    remove a bit of versioning, add sudo, change perms

--- prep-host.sh
+++ prep-host.sh
@@ -73,33 +73,35 @@ if [[ "${HOSTNAME}" == *"mdw"* ]] ; then
     # Stage the GPDB tarball
     curl -o /home/gpadmin/greenplum-db-4.3.8.1-build-1-RHEL5-x86_64.zip  -d "" -H "Authorization: Token iCcyGdFCe5FnR1sFqJ1w" -L https://network.pivotal.io/api/v2/products/pivotal-gpdb/releases/1683/product_files/4367/download
 
-    unzip /home/gpadmin/greenplum-db-4.3.8.1-build-1-RHEL5-x86_64.zip
+    unzip /home/gpadmin/greenplum-db-*.zip
 
     chown gpadmin:gpadmin /home/gpadmin/greenplum-db-*
     chmod u+x /home/gpadmin/greenplum-db-*
 
-    sed -i 's/more <</cat <</g' /home/gpadmin/greenplum-db-4.3.8.1-build-1-RHEL5-x86_64.bin
-    sed -i 's/agreed=/agreed=1/' /home/gpadmin/greenplum-db-4.3.8.1-build-1-RHEL5-x86_64.bin
-    sed -i 's/pathVerification=/pathVerification=1/' /home/gpadmin/greenplum-db-4.3.8.1-build-1-RHEL5-x86_64.bin
-    sed -i '/defaultInstallPath=/a installPath=${defaultInstallPath}' /home/gpadmin/greenplum-db-4.3.8.1-build-1-RHEL5-x86_64.bin
+    sed -i 's/more <</cat <</g' /home/gpadmin/greenplum-db-*.bin
+    sed -i 's/agreed=/agreed=1/' /home/gpadmin/greenplum-db-*.bin
+    sed -i 's/pathVerification=/pathVerification=1/' /home/gpadmin/greenplum-db-*.bin
+    sed -i '/defaultInstallPath=/a installPath=${defaultInstallPath}' /home/gpadmin/greenplum-db-*.bin
 
-    /home/gpadmin/greenplum-db-4.3.8.1-build-1-RHEL5-x86_64.bin
+    sudo /home/gpadmin/greenplum-db-4.3.8.1-build-1-RHEL5-x86_64.bin
 
     # Stage the GPCC tarball
-    curl -o /home/gpadmin/greenplum-cc-web-2.1.0-build-36-RHEL5-x86_64.zip -d "" -H "Authorization: Token iCcyGdFCe5FnR1sFqJ1w" -L https://network.pivotal.io/api/v2/products/pivotal-gpdb/releases/1748/product_files/4517/download
+    curl -o /home/gpadmin/greenplum-cc-web-2.3.0-build-51-RHEL5-x86_64.zip -d "" -H "Authorization: Token iCcyGdFCe5FnR1sFqJ1w" -L https://network.pivotal.io/api/v2/products/pivotal-gpdb/releases/1683/product_files/5097/download
 
-    unzip /home/gpadmin/greenplum-cc-web-2.1.0-build-36-RHEL5-x86_64.zip
+    unzip /home/gpadmin/greenplum-cc-web-*.zip
 
-    chown gpadmin:gpadmin /home/gpadmin/greenplum-db-appliance-*
-    chmod u+x /home/gpadmin/greenplum-db-appliance-*
+    chown gpadmin:gpadmin /home/gpadmin/greenplum-cc-web-*
+    chmod u+x /home/gpadmin/greenplum-cc-web-*
 
-    sed -i 's/more <</cat <</g' /home/gpadmin/greenplum-cc-web-2.1.0-build-36-RHEL5-x86_64.bin
-    sed -i 's/agreed=/agreed=1/' /home/gpadmin/greenplum-cc-web-2.1.0-build-36-RHEL5-x86_64.bin
-    sed -i 's/pathVerification=/pathVerification=1/' /home/gpadmin/greenplum-cc-web-2.1.0-build-36-RHEL5-x86_64.bin
-    sed -i '/defaultInstallPath=/a installPath=${defaultInstallPath}' /home/gpadmin/greenplum-cc-web-2.1.0-build-36-RHEL5-x86_64.bin
+    sed -i 's/more <</cat <</g' /home/gpadmin/greenplum-cc-web-*.bin
+    sed -i 's/agreed=/agreed=1/' /home/gpadmin/greenplum-cc-web-*.bin
+    sed -i 's/pathVerification=/pathVerification=1/' /home/gpadmin/greenplum-cc-web-*.bin
+    sed -i '/defaultInstallPath=/a installPath=${defaultInstallPath}' /home/gpadmin/greenplum-cc-web-*.bin
     
-    /home/gpadmin/greenplum-cc-web-2.1.0-build-36-RHEL5-x86_64.bin
 
+    sudo /home/gpadmin/greenplum-cc-web-2.3.0-build-51-RHEL5-x86_64.bin
+
+    sudo chown -R gpadmin:gpadmin /usr/local/greenplum*
     # Create a cluster deploy hostfile
     python -c "print 'mdw' ; print '\n'.join(['sdw{0}'.format(n+1) for n in range(${SEGMENTS})])" > /home/gpadmin/hostfile
 
